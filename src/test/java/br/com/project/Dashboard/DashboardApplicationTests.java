87587a2ed661e56f119e04c229415b3a569803f7
Nilo <nilogabriel21@gmail.com>
2025-06-15 11:10:29 -0300

    Initialize the Project

---
+++ src/test/java/br/com/project/Dashboard/DashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.project.Dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
